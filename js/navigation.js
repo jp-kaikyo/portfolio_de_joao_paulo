e2153da9e5a217a965ef64021eb5e7a4fdcee0ab
joao paulo <joaopaulopintoqueiros4pgmail.com>
2026-01-12 23:03:26 +0100

    V2.03 = JS : création fichier navigation pour réparer les liens entre pages

---
+++ js/navigation.js
@@ -0,0 +1,48 @@
+///======================///
+/// ==== navigation ==== ///
+///======================///
+
+
+/// INFO : navigation presentation  ///
+
+document.addEventListener("DOMContentLoaded", function() {
+    btn_nav_presentation.onclick = function() {
+        window.location ='presentation.html';  
+    }
+})
+
+
+/// INFO : navigation sommaire  ///
+
+document.addEventListener("DOMContentLoaded", function() {
+    btn_nav_sommaire.onclick = function() {
+        window.location ='sommaire.html';  
+    }
+})
+
+
+/// INFO : navigation documents  ///
+
+document.addEventListener("DOMContentLoaded", function() {
+    btn_nav_documents.onclick = function() {
+        window.location ='documents.html';  
+    }
+})
+
+
+/// INFO : navigation competences  ///
+
+document.addEventListener("DOMContentLoaded", function() {
+    btn_nav_competences.onclick = function() {
+        window.location ='competences.html';  
+    }
+})
+
+
+/// INFO : navigation projets  ///
+
+document.addEventListener("DOMContentLoaded", function() {
+    btn_nav_projets.onclick = function() {
+        window.location ='projets.html';  
+    }
+})
